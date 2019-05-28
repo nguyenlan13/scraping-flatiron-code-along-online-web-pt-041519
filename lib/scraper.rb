b8c3d078bd7c6841cfa6024004b967786f4e3bc6
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-28 07:31:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
        # course.schedule = post.css(".date").text
        # course.description = post.css("p").text
        # binding.pry
-      end
+      #end
     end
     
     def get_courses
